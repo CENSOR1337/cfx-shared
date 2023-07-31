e6ef413709e1aa80c7e861f99fbc20b773824f81
CENSOR_1337 <viixxivvimm@gmail.com>
2023-07-31 10:48:46 +0700

    add global Citizen

---
+++ src/Citizen.ts
@@ -0,0 +1,34 @@
+export class Citizen {
+    public static addRawEventListener = global.addRawEventListener;
+	public static addEventListener = global.addEventListener;
+    public static on = global.on;
+    public static addEventHandler = global.AddEventHandler;
+	public static addNetEventListener = global.addNetEventListener;
+    public static onNet = global.onNet;
+    public static emit = global.emit;
+    public static triggerEvent = global.TriggerEvent;
+    public static emitNet = global.emitNet;
+    public static triggerServerEvent = global.TriggerServerEvent;
+    public static triggerLatentServerEvent = global.TriggerLatentServerEvent;
+    public static getPlayerIdentifiers = global.getPlayerIdentifiers;
+    public static getPlayerTokens = global.getPlayerTokens;
+    public static getPlayers = global.getPlayers;
+    public static sendNuiMessage = global.SendNUIMessage;
+	public static triggerClientEvent = global.TriggerClientEvent;
+    public static triggerLatentClientEvent = global.TriggerLatentClientEvent;
+	public static removeEventListener = global.removeEventListener;
+    public static setTimeout = global.setTimeout;
+    public static clearTimeout = global.clearTimeout;
+    public static setInterval = global.setInterval;
+    public static clearInterval = global.clearInterval;
+    public static setImmediate = global.setImmediate;
+    public static clearImmediate = global.clearImmediate;
+    public static setTick = global.setTick;
+    public static clearTick = global.clearTick;
+    public static newStateBag = global.NewStateBag;
+    public static entity = global.Entity;
+    public static globalState = global.GlobalState;
+    public static player = global.Player;
+    public static localPlayer = global.LocalPlayer;
+	public static exports = global.exports;
+}
